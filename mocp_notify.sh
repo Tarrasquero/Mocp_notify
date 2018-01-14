bc4d46a20cc5336ae7789132e41a325a5b608389
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-01-15 00:56:24 +0100

    Update mocp_notify.sh

--- mocp_notify.sh
+++ mocp_notify.sh
@@ -13,12 +13,17 @@ IMG=`find *.jpg -type f`
 SONGTITLE=$(mocp -Q %song)
 ARTIST=$(mocp -Q %artist)
 FILE=$(echo `mocp -i|grep File:|cut -d '/' -f 6`)
-#ESTADO=$(mocp -Q %state) 
-
+X=$(mat --display "$IMG"|grep "Exif Image Width:"|cut -d ' ' -f 4)
+Y=$(mat --display "$IMG"|grep "Exif Image Height:"|cut -d ' ' -f 4)
+GEOMETRY=$(echo "$X"x"$Y")
+i="100x100"
+if [ $i != $GEOMETRY ]; then
+    mogrify -resize 100x100! "$IMG"
+fi    
 
 if [ "$SONGTITLE" ]; then
     notify-send --icon="$LAST/$IMG" "$ARTIST:" "$SONGTITLE"
-    else
+else    
     notify-send --icon="$LAST/$IMG" "$FILE" #Para musica sin metadatos
 fi
 
