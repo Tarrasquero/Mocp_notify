4999dfe63924a137bfd194e27c96d9a5546d1b65
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-01-19 20:57:51 +0100

    Update mocp_notify.sh

--- mocp_notify.sh
+++ mocp_notify.sh
@@ -5,26 +5,23 @@
 #IRC: Freenode. Channels: #debian-es; #devuan-mx; #archlinux-es.
 #Puedes mejorarlo?. Házmelo saber: yopuebla@gmail.com
 #Configuración en config
-#OnSongChange = "$HOME/scripts/mocp_notify.sh %t %a"
-PWD=$HOME/Musica
-LAST=$(echo "$PWD/`mocp -i|grep File:|cut -d '/' -f 5`")
-cd "$LAST"
-IMG=`find *.jpg -type f`
-SONGTITLE=$(mocp -Q %song)
-ARTIST=$(mocp -Q %artist)
-FILE=$(echo `mocp -i|grep File:|cut -d '/' -f 6`)
+#OnSongChange = "$HOME/scripts/mocp_notify.sh %a %t %f"
+IMG0=`find *.jpg -type f`
+cp "$LAST/$IMG0" "/tmp/cover.jpg"
+IMG=/tmp/cover.jpg
 X=$(mat --display "$IMG"|grep "Exif Image Width:"|cut -d ' ' -f 4)
 Y=$(mat --display "$IMG"|grep "Exif Image Height:"|cut -d ' ' -f 4)
 GEOMETRY=$(echo "$X"x"$Y")
 i="100x100"
+
 if [ $i != $GEOMETRY ]; then
     mogrify -resize 100x100! "$IMG"
 fi    
 
-if [ "$SONGTITLE" ]; then
-    notify-send --icon="$LAST/$IMG" "$ARTIST:" "$SONGTITLE"
+if [ "$2" ]; then
+    notify-send -i "$IMG" "$1:" "$2"
 else    
-    notify-send --icon="$LAST/$IMG" "$FILE" #Para musica sin metadatos
+    notify-send --icon="$IMG" "$3" #Para musica sin metadatos
 fi
 
 exit
