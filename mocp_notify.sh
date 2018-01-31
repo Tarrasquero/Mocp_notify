771713850d1cfa0460f128068cb952769af896a3
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-02-01 00:12:10 +0100

    Update mocp_notify.sh

--- mocp_notify.sh
+++ mocp_notify.sh
@@ -5,38 +5,38 @@
 #Puedes mejorarlo?. Házmelo saber: yopuebla@gmail.com
 #Configuración en config
 #OnSongChange = "$HOME/.moc/scripts/mocp_notify.sh %a %t %f"
-#SONG=$(mocp -Q %song)
-#ARTIST=$(mocp -Q %artist)
+SONG=$(mocp -Q %song)
+ARTIST=$(mocp -Q %artist)
 #FILE=$(mocp -i|grep File:|cut -d '/' -f 6)
 PWD=$HOME/Musica/
-LAST=$(echo "$PWD`mocp -i|grep File:|cut -d '/' -f 2-5`")
 cover="cover.jpg"
+LAST=$(echo "/`mocp -i|grep File:|cut -d '/' -f 2-5`")
 cd "$LAST"
 IMG0=$(ls|grep -i *.jpg)
-IMG=$(echo '"$LAST/$cover"'
-Bits=$(du $IMG0|awk '{print $1)')
+IMG=$(echo "$LAST/$cover")
+Bits=$(du $IMG0|awk '{print $1}')
 
-if [ ! $Bits ]; then
+if [ $Bits -gt "100" ]; then
     
     $HOME/.moc/scripts/Resice.sh
 
 fi    
 
-if [ $cover != $IMG0 ]; then
+if [ $IMG0 != $cover ]; then
 
-    notify-send --icon="$HOME/.moc/scripts/icon-moc.png" "$1:" "$2" #Para musica sin cover
+    notify-send --icon="$HOME/.moc/scripts/icon-moc.png" "$1:" "$2\n$4" #Para musica sin cover
 
-elif [ ! "$2" ] || [ ! "$IMG0" ]; then
+elif [ != $2] || [ ! "$IMG0" ]; then
 
     notify-send --icon="$HOME/.moc/scripts/icon-moc.png" "$3" #Para musica sin metadatos y sin cover 
 
-elif [ ! "$2" ]; then
+elif [ != $2 ]; then
 
     notify-send --icon="$IMG" "$3"
 
-elif [ "$2" ]; then
+elif [ "$SONG" ]; then
 
-    notify-send --icon="$IMG" "$1:" "$2"
+    notify-send -i "$IMG" "$1:" "$2\n$4"
 
 fi
 exit
