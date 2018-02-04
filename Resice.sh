6ff2f5d31a4680cb5468ac8c638de697525a93dc
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-02-04 02:55:17 +0100

    Update Resice.sh

--- Resice.sh
+++ Resice.sh
@@ -1,9 +1,11 @@
-IMG=/tmp/cover.jpg
+LAST=$(echo "/`mocp -i|grep File:|cut -d '/' -f 2-5`")
+cover="cover.jpg"
+IMG=$(echo "$LAST/$cover")
 X=$(mat --display "$IMG"|grep "Exif Image Width:"|cut -d ' ' -f 4)
 Y=$(mat --display "$IMG"|grep "Exif Image Height:"|cut -d ' ' -f 4)
 GEOMETRY=$(echo "$X"x"$Y")
 i="100x100"
 
 if [ $i != $GEOMETRY ]; then
-    mogrify -resize 100x100! $IMG
+    mogrify -adaptive-resize 100x100! "$IMG"
 fi    
