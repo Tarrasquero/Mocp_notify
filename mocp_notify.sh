86202951d996e7548a280e7202ed5ac48d41424e
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-02-05 18:27:39 +0100

    Update mocp_notify.sh

--- mocp_notify.sh
+++ mocp_notify.sh
@@ -8,36 +8,28 @@
 #SONG=$(mocp -Q %song)
 #ARTIST=$(mocp -Q %artist)
 #FILE=$(mocp -i|grep File:|cut -d '/' -f 6)
-cover="cover.jpg"
 LAST=$(echo "/`mocp -i|grep File:|cut -d '/' -f 2-5`")
 cd "$LAST"
-IMG0=$(ls|grep -i cover.jpg)
+IMG0=$(ls|grep -i -e *.jpg -e *.png -e *.jpeg)
 X=$(mat --display "$IMG0"|grep "Exif Image Width:"|cut -d ' ' -f 4)
 
 if [ "$X" -ge "100" ]; then
-    
     $HOME/.moc/scripts/Resice.sh
-
 fi
+
+for cover in *.*; do
 
 if [ "$IMG0" ]; then
-    
     IMG=$(echo "$LAST/$cover")
-
 else
-
     IMG=$(echo "$HOME/.moc/scripts/icon-moc.png")
-
 fi
 
+done
 
 if [ "$2" ]; then
-
     notify-send -i "$IMG" "$1:" "$2\n$4"
-
 else
-
     notify-send -i "$IMG" "$3"
-
 fi
 exit
