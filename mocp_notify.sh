b126aa08ec8eb6e271c8d4d153bc677aa97e056d
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-02-07 21:11:09 +0100

    Update mocp_notify.sh

--- mocp_notify.sh
+++ mocp_notify.sh
@@ -10,14 +10,20 @@
 #FILE=$(mocp -i|grep File:|cut -d '/' -f 6)
 LAST=$(echo "/`mocp -i|grep File:|cut -d '/' -f 2-5`")
 cd "$LAST"
-IMG0=$(ls|grep -i -e *.jpg -e *.png -e *.jpeg)
+I=$(ls|grep -i -e *.jpg)
+if [ -f "$I" ]; then
+    IMG0=$(find . -name '*.jpg'|shuf -n1|xargs -0) #$(ls|grep -i -e *.jpg)  #$(ls|grep -i -e *.jpg -e *.png -e *.jpeg)
+else
+    IMG0=$(find . -name '*.jpg' |cut -c2-|shuf -n1|xargs -0)   
+fi
+ 
 X=$(mat --display "$IMG0"|grep "Exif Image Width:"|cut -d ' ' -f 4)
 
 if [ "$X" -ge "100" ]; then
     $HOME/.moc/scripts/Resice.sh
 fi
 
-for cover in *.*; do
+for cover in "$IMG0"; do
 
 if [ "$IMG0" ]; then
     IMG=$(echo "$LAST/$cover")
