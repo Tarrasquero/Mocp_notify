b7f89f10a7d044a5bf59a6649d63b0c2d3e65f93
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-02-08 19:29:44 +0100

    Update mocp_notify.sh

--- mocp_notify.sh
+++ mocp_notify.sh
@@ -12,9 +12,9 @@ LAST=$(echo "/`mocp -i|grep File:|cut -d '/' -f 2-5`")
 cd "$LAST"
 I=$(ls|grep -i -e *.jpg)
 if [ -f "$I" ]; then
-    IMG0=$(find . -name '*.jpg'|shuf -n1|xargs -0) #$(ls|grep -i -e *.jpg)  #$(ls|grep -i -e *.jpg -e *.png -e *.jpeg)
+    IMG0=$(find . -name '*.jpg' -or -name '*.jpeg' -or -name '*.png'|shuf -n1|xargs -0) #$(ls|grep -i -e *.jpg)  #$(ls|grep -i -e *.jpg -e *.png -e *.jpeg)
 else
-    IMG0=$(find . -name '*.jpg' |cut -c2-|shuf -n1|xargs -0)   
+    IMG0=$(find . -name '*.jpg' -or -name '*.jpeg' -or -name '*.png'|cut -c2-|shuf -n1|xargs -0)   
 fi
  
 X=$(mat --display "$IMG0"|grep "Exif Image Width:"|cut -d ' ' -f 4)
