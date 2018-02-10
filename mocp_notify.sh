b4a3f2a0f58e6bf0e36b268cf8861a643778e737
Tarrasquero <35396143+Tarrasquero@users.noreply.github.com>
2018-02-10 15:14:04 +0100

    Update mocp_notify.sh

--- mocp_notify.sh
+++ mocp_notify.sh
@@ -10,27 +10,24 @@
 #FILE=$(mocp -i|grep File:|cut -d '/' -f 6)
 LAST=$(echo "/`mocp -i|grep File:|cut -d '/' -f 2-5`")
 cd "$LAST"
+X=$(mat --display "$IMG0"|grep "Exif Image Width:"|cut -d ' ' -f 4)
+if [ "$X" != "100" ]; then
+    $HOME/.moc/scripts/Resize.sh
+fi
+
 I=$(ls|grep -i -e *.jpg)
 if [ -f "$I" ]; then
     IMG0=$(find . -name '*.jpg' -or -name '*.jpeg' -or -name '*.png'|shuf -n1|xargs -0)
 else
     IMG0=$(find . -name '*.jpg' -or -name '*.jpeg' -or -name '*.png'|cut -c2-|shuf -n1|xargs -0)   
 fi
- 
-X=$(mat --display "$IMG0"|grep "Exif Image Width:"|cut -d ' ' -f 4)
-
-if [ "$X" != "100" ]; then
-    $HOME/.moc/scripts/Resize.sh
-fi
 
 for cover in "$IMG0"; do
-
 if [ "$IMG0" ]; then
     IMG=$(echo "$LAST/$cover")
 else
     IMG=$(echo "$HOME/.moc/scripts/icon-moc.png")
 fi
-
 done
 
 if [ "$2" ]; then
